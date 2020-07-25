f563f6227a15355b945f82a700c7224415ec7812
nprindle <nprindle18@gmail.com>
2020-07-25 15:42:42 -0400

    Switch to gtcs2110/cs2110docker, clean up cs2110docker.sh

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# cs2110docker 
+# cs2110docker
 
-This Docker image is used by students enrolled in CS 2110 at the Georgia Institute of Technology. It contains an Ubuntu 18.04 image
-that contains the necessary tools for the class (gcc/gdb, complx, vba), along with a VNC server and noVNC to allow students to remotely
-access it from their browser. 
+This Docker image is used by students enrolled in CS 2110 at the Georgia
+Institute of Technology. It contains an Ubuntu 18.04 image that contains the
+necessary tools for the class (gcc/gdb, complx, vba), along with a VNC server
+and noVNC to allow students to remotely access it from their browser.
 
 In order to run this image, install Docker and run the cs2110docker.sh script.
 
-Find the Docker Image on Docker Hub at  at https://hub.docker.com/r/gibrane/cs2110docker/.
+Find the Docker Image on Docker Hub at https://hub.docker.com/r/gtcs2110/cs2110docker
 
-
-Credit to https://github.com/ConSol/docker-headless-vnc-container for initial setup
+Credit to https://github.com/ConSol/docker-headless-vnc-container for initial
+setup
--- cs2110docker.sh
+++ cs2110docker.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-echo THIS IS THE NEW SCRIPT
-
-imageName="gibrane/cs2110docker"
+imageName="gtcs2110/cs2110docker"
 
 docker -v >/dev/null
 
 dockerExists=$?
 
 if  [ $dockerExists != 0 ]; then
-	echo ERROR: Please install Docker before running this script. Refer to the CS 2110 Docker Guide.
-	exit
+  >&2 echo "ERROR: Please install Docker before running this script. Refer to the CS 2110 Docker Guide."
+  exit 1
 fi
 
 docker container ls >/dev/null
 dockerNotRunning=$?
 
 if [ $dockerNotRunning != 0 ]; then
-	echo ERROR: Docker is not currently running. Please start Docker before running this script.
-	exit
+  >&2 echo "ERROR: Docker is not currently running. Please start Docker before running this script."
+  exit 1
 fi
 
-echo Found Docker Installation. Checking for existing containers.
+echo "Found Docker Installation. Checking for existing containers."
 
 existingContainers=$(docker ps -a | grep $imageName | awk '{print $1}')
 echo $existingContainers
 if [[ -n $existingContainers ]]; then
-	echo Found CS 2110 containers. Stopping and removing them.
-	docker stop $existingContainers >/dev/null
-	docker rm $existingContainers >/dev/null
-else 
-	echo No existing CS 2110 containers.
+  echo "Found CS 2110 containers. Stopping and removing them."
+  docker stop $existingContainers >/dev/null
+  docker rm $existingContainers >/dev/null
+else
+  echo "No existing CS 2110 containers."
 fi
 
-if [ ! -z "$1" ] && [ $1 == "stop" ]; then
-	echo Successfully stopped CS 2110 containers
-	exit 
-fi 
+if [ -n "$1" ] && [ "$1" == "stop" ]; then
+  echo "Successfully stopped CS 2110 containers"
+  exit
+fi
 
 echo Pulling down most recent image of $imageName
 docker pull $imageName
@@ -44,32 +42,29 @@ docker pull $imageName
 successfulPull=$?
 
 if [ $successfulPull != 0 ]; then
-	echo ERROR: Unable to pull down the most recent image of $imageName
-fi 
+  >&2 echo "ERROR: Unable to pull down the most recent image of $imageName"
+fi
 
-echo Starting up new CS 2110 Docker Container:
+echo "Starting up new CS 2110 Docker Container:"
 
 ipAddress="$(docker-machine ip default 2>/dev/null)"
-foundIp=$?;
+foundIp=$?
 
 if [ $foundIp != 0 ]; then
-	ipAddress="127.0.0.1";
+  ipAddress="127.0.0.1";
 fi
 
 if [ "$1" == "-it" ]; then
-	docker run --rm -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it  --entrypoint /bin/bash gibrane/cs2110docker
-else 
-	docker run -d -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined gibrane/cs2110docker 
+  docker run --rm -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it  --entrypoint /bin/bash "$imageName"
+else
+  docker run -d -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$imageName"
 
-	successfulRun=$?
+  successfulRun=$?
 
-	
-
-	
-	if [ $successfulRun == 0 ]; then
-		echo Successfully launched CS 2110 Docker container. Please go to http://$ipAddress:6901/vnc.html to access it.
-	else 
-		echo ERROR: Unable to launch CS 2110 Docker container.
-	fi 
+  if [ $successfulRun == 0 ]; then
+    echo "Successfully launched CS 2110 Docker container. Please go to http://$ipAddress:6901/vnc.html to access it."
+  else
+    >&2 echo "ERROR: Unable to launch CS 2110 Docker container."
+  fi
 fi
 
