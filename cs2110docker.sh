c3ece0920a1954f8db7136e498c5a06c55248876
Nicole Prindle <nprindle18@gmail.com>
2020-08-15 12:20:58 -0400

    Clean up cs2110docker.sh

--- cs2110docker.sh
+++ cs2110docker.sh
@@ -23,7 +23,7 @@ echo "Found Docker Installation. Checking for existing containers."
 
 existingContainers=$(docker ps -a | grep $imageName | awk '{print $1}')
 echo $existingContainers
-if [[ -n $existingContainers ]]; then
+if [ -n "$existingContainers" ]; then
   echo "Found CS 2110 containers. Stopping and removing them."
   docker stop $existingContainers >/dev/null
   docker rm $existingContainers >/dev/null
@@ -31,7 +31,7 @@ else
   echo "No existing CS 2110 containers."
 fi
 
-if [ -n "$1" ] && [ "$1" == "stop" ]; then
+if [ -n "$1" ] && [ "$1" = "stop" ]; then
   echo "Successfully stopped CS 2110 containers"
   exit
 fi
@@ -51,17 +51,17 @@ ipAddress="$(docker-machine ip default 2>/dev/null)"
 foundIp=$?
 
 if [ $foundIp != 0 ]; then
-  ipAddress="127.0.0.1";
+  ipAddress="127.0.0.1"
 fi
 
-if [ "$1" == "-it" ]; then
+if [ "$1" = "-it" ]; then
   docker run --rm -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it  --entrypoint /bin/bash "$imageName"
 else
   docker run -d -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$imageName"
 
   successfulRun=$?
 
-  if [ $successfulRun == 0 ]; then
+  if [ $successfulRun = 0 ]; then
     echo "Successfully launched CS 2110 Docker container. Please go to http://$ipAddress:6901/vnc.html to access it."
   else
     >&2 echo "ERROR: Unable to launch CS 2110 Docker container."
