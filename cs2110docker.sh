aa8683f9bf879bef1e1d056dbb4098aaf64e4d98
nprindle <nprindle18@gmail.com>
2020-09-14 14:49:36 -0400

    Fix host folder binding for Windows Docker Desktop
    Unix-like paths as printed by `pwd` in Git Bash are incompatible with
    Docker Desktop on Windows; they cause host binding to fail and create
    empty dummy directories like 'cs2110;C'. Instead, we use `pwd -W` to
    format it as a Windows 'C:'-style path, and fall back on `pwd` if that's
    unavailable.

--- cs2110docker.sh
+++ cs2110docker.sh
@@ -54,10 +54,13 @@ if [ $foundIp != 0 ]; then
   ipAddress="127.0.0.1"
 fi
 
+# -W should correct path incompatibilites on Windows for Docker Desktop users
+currDir="$(pwd -W 2>/dev/null || pwd)"
+
 if [ "$1" = "-it" ]; then
-  docker run --rm -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it  --entrypoint /bin/bash "$imageName"
+  docker run --rm -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$currDir":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it  --entrypoint /bin/bash "$imageName"
 else
-  docker run -d -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$(pwd)":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$imageName"
+  docker run -d -p $ipAddress:6901:6901 -p $ipAddress:5901:5901 -v "$currDir":/cs2110/host/ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined "$imageName"
 
   successfulRun=$?
 
