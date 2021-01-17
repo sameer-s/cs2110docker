622ed9bd3b9f34bfd4cb25d6c9b6d9d59e776984
Nicole Prindle <nprindle18@gmail.com>
2021-01-16 22:53:23 -0500

    Unpin openjfx to fix build
    Consider using something like sdkman in the future for java/openjfx
    installation; I really don't like having things unpinned

--- src/install/tools/circuitsimJava.sh
+++ src/install/tools/circuitsimJava.sh
@@ -3,4 +3,6 @@
 set -e
 
 echo "Install circuitsim"
-apt-get update && apt-get install -y openjdk-8-jre xserver-xorg-video-dummy openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
+apt-get update && apt-get install -y openjdk-8-jre xserver-xorg-video-dummy
+# apt-get update && apt-get install -y openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
+apt-get update && apt-get install -y openjfx libopenjfx-java libopenjfx-jni
