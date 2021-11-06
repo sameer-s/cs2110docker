8772411ae7ee7918d237e36645fe49267a6935ac
Nicole Prindle <nprindle18@gmail.com>
2021-11-06 16:08:24 -0400

    Add minimal text editors
    Resolves #11

--- src/install/base/apps.sh
+++ src/install/base/apps.sh
@@ -7,5 +7,9 @@ apt-get update
 # PDF viewer (for Complx documentation in particular)
 apt-get install -y evince
 
+# minimal text editors, mostly for TA/professor use
+# students generally shouldn't be editing inside the container
+apt-get install -y vim-tiny nano
+
 apt-get clean -y
 
