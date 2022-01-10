43b88b549cb9953345e6c07defde256a2c7f195e
Brandon <TricksterGuy@users.noreply.github.com>
2022-01-10 14:56:05 -0800

    Replace mednafen with mgba. (#8)
    Mednafen isn't a good GBA emulator and isn't meant to be, it is buggy (crashes on big DMAs), inaccurate, and has no actual debugging functionality that other GBA emulators have. (No sprite/memory/etc viewers, no gdb functionality, etc.)
    
    Switch mednafen to mgba 0.9.3; the correspond Makefiles and documentation around the GBA homework will also need to be updated.
    
    The following Makefile should be sufficient for future GBA assignments:
    
    ```make
    PROGNAME = App
    
    OFILES = gba.o font.o main.o images/garbage.o
    
    .PHONY: all
    all: CFLAGS += $(CRELEASE) -I../shared
    all: LDFLAGS += $(LDRELEASE)
    all: $(PROGNAME).gba
    	@echo "[FINISH] Created $(PROGNAME).gba"
    
    include /opt/cs2110-tools/GBAVariables.mak
    
    LDFLAGS += --specs=nosys.specs
    CFLAGS += -Wstrict-prototypes -Wold-style-definition
    
    .PHONY: debug
    debug: CFLAGS += $(CDEBUG) -I../shared
    debug: LDFLAGS += $(LDDEBUG)
    debug: $(PROGNAME).gba
    	@echo "[FINISH] Created $(PROGNAME).gba"
    
    $(PROGNAME).gba: $(PROGNAME).elf
    	@echo "[LINK] Linking objects together to create $(PROGNAME).gba"
    	@$(OBJCOPY) -O binary $(PROGNAME).elf $(PROGNAME).gba
    
    $(PROGNAME).elf: crt0.o $(GCCLIB)/crtbegin.o $(GCCLIB)/crtend.o $(GCCLIB)/crti.o $(GCCLIB)/crtn.o $(OFILES) libc_sbrk.o
    	$(CC) -o $(PROGNAME).elf $^ $(LDFLAGS)
    
    .PHONY: mgba
    mgba: CFLAGS += $(CRELEASE) -I../shared
    mgba: LDFLAGS += $(LDRELEASE)
    mgba: $(PROGNAME).gba
    	@echo "[EXECUTE] Running emulator mGBA"
    	@echo "          Please see emulator.log if this fails"
    	@mgba-qt $(PROGNAME).gba
    
    .PHONY: gdb
    gdb: debug
    	@echo "[EXECUTE] Running Emulator MGBA with GDB"
    	@echo "          Please see emulator.log if this fails"
    	@mgba-qt $(PROGNAME).gba --gdb &
    	# To give mgba some time to initialize and start up the GDB server.
    	@sleep 0.5
    	@gdb-multiarch -ex 'file $(PROGNAME).elf' -ex 'target remote 127.0.0.1:2345'
    
    .PHONY: submit
    submit: clean
    	@rm -f submission.tar.gz
    	@tar czvf submission.tar.gz *
    
    .PHONY: clean
    clean:
    	@echo "[CLEAN] Removing all compiled files"
    	rm -f *.o *.elf *.gba *.log */*.o *.sav
    ```
    
    Co-authored-by: Nicole Prindle <nprindle18@gmail.com>

--- src/install/tools/complxGba.sh
+++ src/install/tools/complxGba.sh
@@ -11,9 +11,21 @@ add-apt-repository ppa:tricksterguy87/nin10kit
 apt-get update
 
 apt-get install -y complx-tools nin10kit
-apt-get install -y gcc-arm-none-eabi gdb-multiarch mednafen
+apt-get install -y gcc-arm-none-eabi gdb-multiarch
 
-# GBA linker script
+# GBA linker script; random oddities needed in the GBA Makefiles.
 dpkg -i $INST_SCRIPTS/pkgs/cs2110-gba-linker-script_1.1.2-0ubuntu1~ppa1~bionic1_amd64.deb
 rm $INST_SCRIPTS/pkgs/cs2110-gba-linker-script_1.1.2-0ubuntu1~ppa1~bionic1_amd64.deb
+
+# MGBA emulator
+# Unfortunately apt-get install mgba-qt will get you a very outdated version
+# the deb packages are released directly on github
+apt-get install -y $INST_SCRIPTS/pkgs/libmgba_0.9.3-focal.deb
+apt-get install -y $INST_SCRIPTS/pkgs/mgba-qt_0.9.3-focal.deb
+rm $INST_SCRIPTS/pkgs/libmgba_0.9.3-focal.deb
+rm $INST_SCRIPTS/pkgs/mgba-qt_0.9.3-focal.deb
+
+# Necessary config to get mgba to not run super fast, custom button keymapping, and other niceties.
+mkdir -p $HOME/.config/mgba
+cp $INST_SCRIPTS/configs/mgba-config.ini $HOME/.config/mgba/config.ini
 
