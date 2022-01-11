27410f5a607357dd420b6f742ee5e071710a3b7f
Sameer Suri <git@sameersuri.com>
2022-01-11 08:45:20 -0500

    fixed arm

--- src/install/base/xfce_ui.sh
+++ src/install/base/xfce_ui.sh
@@ -4,6 +4,13 @@ set -e
 
 echo "Install Xfce4 UI components"
 apt-get update
+
+# Install new version of xfwm4 manually to avoid this bug
+# https://bugs.launchpad.net/ubuntu/+source/xfwm4/+bug/1860921
+[[ $(dpkg --print-architecture) == "amd64" ]] && ARCH="amd64" || ARCH="arm64"
+apt-get install -y $INST_SCRIPTS/pkgs/xfwm4_4.14.5-1_$ARCH.deb
+rm $INST_SCRIPTS/pkgs/xfwm4_4.14.5-1_*.deb
+
 apt-get install -y supervisor xfce4 xfce4-terminal
 apt-get purge -y pm-utils xscreensaver*
 apt-get clean -y
--- src/install/tools/complxGba.sh
+++ src/install/tools/complxGba.sh
@@ -20,10 +20,22 @@ rm $INST_SCRIPTS/pkgs/cs2110-gba-linker-script_1.1.2-0ubuntu1~ppa1~bionic1_amd64
 # MGBA emulator
 # Unfortunately apt-get install mgba-qt will get you a very outdated version
 # the deb packages are released directly on github
-apt-get install -y $INST_SCRIPTS/pkgs/libmgba_0.9.3-focal.deb
-apt-get install -y $INST_SCRIPTS/pkgs/mgba-qt_0.9.3-focal.deb
+if [[ $(dpkg --print-architecture) == "arm64" ]]; then
+    # No package for ARM so I had to build from source
+    # TODO: find a better solution
+
+    # Install dependencies
+    apt-get install -y libsdl2-2.0-0 libopengl0 libqt5widgets5 libqt5core5a libqt5gui5 libqt5multimedia5 libqt5opengl5 libzip5
+    
+    # Extract tarball with binaries
+    tar -zxvf $INST_SCRIPTS/pkgs/mgba_arm64.tar.gz -C /usr
+else
+    apt-get install -y $INST_SCRIPTS/pkgs/libmgba_0.9.3-focal.deb
+    apt-get install -y $INST_SCRIPTS/pkgs/mgba-qt_0.9.3-focal.deb
+fi
 rm $INST_SCRIPTS/pkgs/libmgba_0.9.3-focal.deb
 rm $INST_SCRIPTS/pkgs/mgba-qt_0.9.3-focal.deb
+rm $INST_SCRIPTS/pkgs/mgba_arm64.tar.gz
 
 # Necessary config to get mgba to not run super fast, custom button keymapping, and other niceties.
 mkdir -p $HOME/.config/mgba
