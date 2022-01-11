b0f58afee708ee95ca9ff730aeb8266085ba8d22
Sameer Suri <git@sameersuri.com>
2022-01-11 08:48:37 -0500

    Cleanup and adding comments

--- src/install/base/tigervnc.sh
+++ src/install/base/tigervnc.sh
@@ -2,11 +2,5 @@
 set -e
 
 echo "Install VNC server"
-#if [[ $(dpkg --print-architecture) == "arm64" ]]; then
-#	wget -P $INST_SCRIPTS/pkgs https://iweb.dl.sourceforge.net/project/tigervnc/stable/1.12.0/ubuntu-20.04LTS/arm64/tigervncserver_1.12.0-1ubuntu1_arm64.deb
-#	apt install -y $INST_SCRIPTS/pkgs/tigervncserver_1.12.0-1ubuntu1_arm64.deb
-#	rm $INST_SCRIPTS/pkgs/tigervncserver_1.12.0-1ubuntu1_arm64.deb
-#else
-#	wget -qO- https://sourceforge.net/projects/tigervnc/files/stable/1.8.0/tigervnc-1.8.0.x86_64.tar.gz/download | tar xz --strip 1 -C /
-#fi
+# Replaced TigerVNC with TightVNC due to having up-to-date ARM builds
 apt install -y tightvncserver
